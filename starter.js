48b4bab1902ec0e60c973fb9a4e006c5502eeda2
Dahlia <dnunn@icstars.org>
2025-10-20 15:15:11 -0500

    DahliaNunn

--- starter.js
+++ starter.js
@@ -3,18 +3,14 @@
 const numbers = [10, 20, 30, 40];
 
 
-let total1 = 0;
+let total = 0;
 for (let num of numbers) {
-  total1 += num;
+  total += num;
 }
-console.log("Total 1:", total1);
-
-let total2 = 0;
-for (let num of numbers) {
-  total2 += num;
-}
-console.log("Total 2:", total2);
+console.log(total);
 
 // Calculate the average
 let average = total / numbers.length;
 console.log("Average:", average);
+
+
